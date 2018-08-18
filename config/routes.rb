6880bc3c3a92ac9c9b23379a310cac1dcec107e8
Waihon Yew <yewwaihon@gmail.com>
2018-08-18 15:39:15 +0800

    Find a cart

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'store#index', as: 'store_index'
 
   resources :products
+  resources :carts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
